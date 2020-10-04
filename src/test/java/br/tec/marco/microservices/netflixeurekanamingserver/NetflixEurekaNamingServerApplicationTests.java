f9cfa17d03a8334d330b488ff268a1ef0bcfcd3b
Marco Yuri Fujii Melo <marcoyf@gmail.com>
2020-10-04 00:31:42 -0300

    initial commit

---
+++ src/test/java/br/tec/marco/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.marco.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
